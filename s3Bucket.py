e8549ba56200ae0b252f179725cdfb84312473e0
m-musila <m.musila@alustudent.com>
2023-08-06 14:24:20 +0400

    update sqs. s3 bucket done

--- s3Bucket.py
+++ s3Bucket.py
@@ -1,22 +1,49 @@
-import logging
+import time
 import boto3
 from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
 
-# Create an S# bucket for the region
-#If no region specified, the bucket is created in the S3 default us-east-1 region.
-def create_bucket(bucket_name, region=None):
-    # Create bucket and error handling
+
+# load enviroment values for the sqs queue.
+load_dotenv() 
+region = os.getenv("REGION_NAME")
+keyId = os.getenv("AWS_ACCESS_KEY_ID")
+key = os.getenv("AWS_SECRET_ACCESS_KEY")
+token = os.getenv("AWS_SESSION_TOKEN")
+
+
+# The bucket is created in the S3 default us-east-1 region. the image uplaod done
+def s3BucketAndFileUpload(image, objectName):
+    # Create an S3 bucket for the region 
+    client = boto3.resource("s3",
+                              region_name = region,
+                              aws_access_key_id = keyId,
+                              aws_secret_access_key = key,
+                              aws_session_token = token 
+                              )
+    # Access image location
+    imageLocation = os.getcwd()
+    # My image bucket
+    bucketName = "s2034964ImgS3Bucket"
+    # Access image name using images folder dir
+    imageName = os.path.join(imageLocation, "images", image)
+
+    # checks for return values and error conditions
     try:
-        if region is None:
-            s3_client = boto3.client('s3')
-            s3_client.create_bucket(Bucket=bucket_name)
-        else:
-            s3_client = boto3.client('s3', region_name=region)
-            location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name,
-                                    CreateBucketConfiguration=location)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    #return: True if bucket created, else False
-    return True
+        # upload images to s3 bucket
+        client.upload_file(objectName, bucketName, imageName)
+        print("Upload was successful")
+    except ClientError as e:  
+        print("could not upload file")
+
+# upload file with 30 seconds intervals. 
+for i in range(1,6):
+
+    #Running loop from 1 to 6
+    image = "image" + str(i) + ".jpg"
+    object = "object" + str(i) + ".jpg" 
+    s3BucketAndFileUpload(image, object)
+
+    # adding 30 seconds time delay
+    time.sleep(30)
--- sqs.py
+++ sqs.py
@@ -11,16 +11,14 @@ keyId = os.getenv("AWS_ACCESS_KEY_ID")
 key = os.getenv("AWS_SECRET_ACCESS_KEY")
 token = os.getenv("AWS_SESSION_TOKEN")
 
-
-
 # create sqs queue resouce from boto3 with the enviroment variables
 def sqsQueue():
     sqsQueue = boto3.resource("sqs",
-                         region_name = region,
-                         aws_access_key_id = keyId,
-                         aws_secret_access_key = key,
-                         aws_session_token = token
-                         )
+                              region_name = region,
+                              aws_access_key_id = keyId,
+                              aws_secret_access_key = key,
+                              aws_session_token = token 
+                              )
    # checks for return values and error conditions
     try:
         # create sqs queue instance from above resource 
@@ -29,5 +27,3 @@ def sqsQueue():
     except ClientError as e:  
         print("sqs queue instance error. Check implementation and try again!")
 
-# Create the queue
-sqsQueue()
